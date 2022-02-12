ce9e52cb601de9a576ea6793cb46c114c7ab7102
Jay <53712717+EungyuCho@users.noreply.github.com>
2022-02-12 23:42:56 +0900

    chore: dockerize :whale:

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'cart',
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   autoLoadEntities: true,
